2bbdbd36ec0261413dc7d7cb0e41fe4d7fea0f2e
g1tka <165782083+g1tka@users.noreply.github.com>
2024-05-14 02:16:14 +0000

    [Finish]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
   get 'homes/about' => 'homes#about', as: 'about'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
 
@@ -37,3 +39,5 @@ end
 #                       user GET    /users/:id(.:format)                          users#show
 #                            PATCH  /users/:id(.:format)                          users#update
 #                            PUT    /users/:id(.:format)                          users#update
+#   post_image_post_comments POST   /post_images/:post_image_id/post_comments(.:format)           post_comments#create
+#    post_image_post_comment DELETE /post_images/:post_image_id/post_comments/:id(.:format)       post_comments#destroy
